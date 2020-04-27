ae8451b2bbfa6fe7c05d990816fd4d471685f86f
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-04-27 18:18:23 -0400

    add: express server

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+module.exports = app;
+
+const createApp = () => {
+  // Logging middleware //
+  app.use(morgan('dev'));
+  // Body-parsing middleware //
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // Static file serving middleware. Uncomment this when ready. //
+  // app.use(express.static(path.join(__dirname, '..', 'public')));
+
+  // Sends our index.html. Uncomment when ready. //
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../public/index.html'));
+  // });
+
+  // Initialize passport //
+
+  // API routes //
+  app.use('/api', require('./api'));
+
+  // Error handling middleware //
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+};
+
+const startListening = () => {
+  // Start listening (and create a 'server' object representing our server)
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+};
+
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
+
+bootApp();
