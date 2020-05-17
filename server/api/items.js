28d23ca50480f8e2f5a4e559acc71275cb156bc0
gjyhuang <grace.jy.huang@gmail.com>
2020-05-17 17:02:57 -0400

    add: filter queries wip

--- server/api/items.js
+++ server/api/items.js
@@ -19,47 +19,61 @@ router.get('/:category/:id', async (req, res, next) => {
 });
 
 router.get('/:category', async (req, res, next) => {
-  // search filters:  try {
-    // const { query } = req;
-    // const keys = Object.keys(query);
-    // if (keys.length) {
-
-    //   const items = await db.model(req.params.category).find(query);
-    //   res.json(items);
-    // } else {
-    //   const items = await db.model(req.params.category).find();
-    //   res.json(items);
-    // }
   try {
     const { query } = req;
     const keys = Object.keys(query);
-    if (keys.length > 2) {
 
-      const items = await db.model(req.params.category).find(query);
-      res.json(items);
-    } else {
-      // pageNum is the nth page of results the user is on
-      // limit is the number of items to appear per page
-      const pageNum = Math.max(0, Number(req.query.page) - 1);
-      const limit = Number(req.query.limit) || 50;
-      const items = await db.model(req.params.category).find().skip(pageNum * limit).limit(limit);
-      res.json(items);
+    // check if there are any filtering queries
+    const keysStr = keys.join('');
+    const filters = {};
+    if (keys.length && keysStr !== 'pagelimit' && keysStr !== 'limitpage') {
+      console.log('we have a query');
+      // queries that apply to all or nearly all items
+      if (keys.includes('diy')) {
+        filters['diy'] = query.diy;
+      }
+      if (keys.includes('color')) {
+        filters['color1'] = query.color;
+        filters['color2'] = query.color;
+      }
+
+      // queries for furniture, decor, flooring/wallpaper
+      if (keys.includes('bodyCustomize')) {
+        filters['bodyCustomize'] = bodyCustomize;
+      }
+      if (keys.includes('hhaConcept')) {
+        filters['hhaConcept1'] = query.hhaConcept;
+        filters['hhaConcept2'] = query.hhaConcept;
+      }
+      if (keys.includes('hhaSeries')) {
+        filters['hhaSeries'] = hhaSeries;
+      }
+      if (keys.includes('patternCustomize')) {
+        filters['patternCustomize'] = patternCustomize;
+      }
+      if (keys.includes('tag')) {
+        filters['tag'] = query.tag;
+      }
+      if (keys.includes('vfx')) {
+        filters['vfx'] = query.vfx;
+      }
+
+      // queries for clothing
+      if (keys.includes('style')) {
+        filters['style'] = query.style;
+      }
+      if (keys.includes('labelThemes')) {
+        filters['labelThemes'] = query.labelThemes;
+      }
     }
+
+    // pageNum is the nth page of results the user is on
+    // limit is the number of items to appear per page
+    const pageNum = Math.max(0, Number(query.page) - 1);
+    const limit = Number(query.limit) || 50;
+    const items = await db.model(req.params.category).find(filters).skip(pageNum * limit).limit(limit);
+    res.json(items);
   } catch (error) {
     next(error);
   }
-});
-
-
-
-router.post('/', (req, res, next) => {
-  res.send('Hello post');
-});
-
-router.put('/:userId', (req, res, next) => {
-  res.send('Hello put');
-});
-
-router.delete('/:userId', (req, res, next) => {
-  res.send('Goodbye world');
 });
